e5407fbd6f7a29c2f65327d4bbc1b6a996c2bff2
sjz <1264946574@qq.com>
2020-01-23 00:12:37 +0800

    尝试对对象列表分割.初步测试

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import json
+import re
 
 from wxpy import *
 bot = Bot(cache_path=True)
 # friend_total = bot.friends().stats_text()
 # print(friend_total)
 all_friends = bot.friends(update=True)
-print(all_friends.search())
+# print(all_friends.search())
+# print(all_friends[1])
+for i in all_friends:
+    s1 = all_friends[i]
+    re.split(s1,r'[:>]')
+    print(s1[1])
