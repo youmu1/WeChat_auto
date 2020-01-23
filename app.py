a479d585ca556150760e6c6a3d9e652b80eae679
JiaZhen Shen <47550916+youmu1@users.noreply.github.com>
2020-01-23 11:07:20 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import json
 import re
-
 from wxpy import *
+
+zf = "新年快乐"
 bot = Bot(cache_path=True)
 # friend_total = bot.friends().stats_text()
 # print(friend_total)
-all_friends = bot.friends(update=True)
-# print(all_friends.search())
-# print(all_friends[1])
-for i in all_friends:
-    s1 = all_friends[i]
-    re.split(s1,r'[:>]')
-    print(s1[1])
+all_friends = bot.friends()
+# print(all_friends)
+i = 0
+while all_friends[i]:
+    s1 = all_friends[i].name
+
+    s1 = s1 + "," + zf
+    print( s1 )
+
+    i = i+1
